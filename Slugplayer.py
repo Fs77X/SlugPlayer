15ae9b16e608685605b8c427962c90c40c1b0e99
Fs77X <farhansaeed1999@gmail.com>
2018-12-21 21:29:01 -0800

    Fully functioning finally :)
    Fully functioning MP3 player (without inserting and deleting)

--- Slugplayer.py
+++ Slugplayer.py
@@ -1,5 +1,9 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QAction
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import Qt
+import os
 import vlc
 from VLCManager import VLCManager
 
@@ -7,19 +11,44 @@ from VLCManager import VLCManager
 class window(QtWidgets.QMainWindow):    
     def __init__(self):
         super(window, self).__init__()
-        idx = 0
+        mylist = self.getList()
+        scriptDir = os.path.dirname(os.path.realpath(__file__))
+        mylist2 = os.listdir(scriptDir + "/songs")
 
-        self._manager = VLCManager(0, 0, 0)
-        self._manager.setFileName("/songs/Immigrant Song.mp3")
-        self.setWindowTitle('SlugPlayer')
-        self.home()
+        if mylist != -1:
+            self._manager = VLCManager(0, 0, mylist, mylist2, len(mylist))
+            self._manager.setFileName(mylist[0])
+            self.setWindowTitle('SlugPlayer')
+            self.setWindowIcon(QtGui.QIcon(scriptDir + os.path.sep + 'slug.png'))
+
+
+            extractAction = QAction('&Quit', self)
+            extractAction.setShortcut('Ctrl+Q')
+            extractAction.triggered.connect(self.close)
+
+            self.statusBar()
+
+            mainMenu = self.menuBar()
+            fileMenu = mainMenu.addMenu('&File')
+            fileMenu.addAction(extractAction)
+
+            self.home()
+        else:
+            self.setWindowTitle("ERROR404")
+            self.setGeometry(200, 100, 300, 100) #x, y (where it initially opens on screen) w, h (size of window)
+            self.setWindowIcon(QtGui.QIcon(scriptDir + os.path.sep + 'cyber-slugeditnotallowed.png'))
+            self.setFixedSize(self.size())
+            self.error()
+
 
     def home(self):
-        quitbtn = QtWidgets.QPushButton('quit')
-        quitbtn.clicked.connect(self.close)
+    
+        # quitbtn = QtWidgets.QPushButton('Quit')
+        # quitbtn.clicked.connect(self.close)
 
         playbtn = QtWidgets.QPushButton('Play')
         playbtn.clicked.connect(self._manager.play)
+        
 
         psebtn = QtWidgets.QPushButton('Pause', self)
         psebtn.clicked.connect(self._manager.pause)
@@ -27,33 +56,83 @@ class window(QtWidgets.QMainWindow):
         stopbtn = QtWidgets.QPushButton('Stop', self)
         stopbtn.clicked.connect(self._manager.stop)
 
-        test1 = QtWidgets.QPushButton('Test1', self)
-        test1.clicked.connect(self._manager.test1)
+        nextbtn = QtWidgets.QPushButton('Next', self)
+        nextbtn.clicked.connect(self._manager.next)
+        prevbtn = QtWidgets.QPushButton('Prev', self)
+        prevbtn.clicked.connect(self._manager.prev)
 
-        test2 = QtWidgets.QPushButton('Test2', self)
-        test2.clicked.connect(self._manager.test2)
-
-        test3 = QtWidgets.QPushButton('Test3', self)
-        test3.clicked.connect(self._manager.test3)
-        rand = QtWidgets.QPushButton('Random', self)
-        rand.clicked.connect(self._manager.randomize)
+        self.playing = QtWidgets.QLabel()
+        self.playing.setText("Nothing Playing")
+        # self.currentP = QtWidgets.QLabel()
+        # self.currentP.setText("0:00:00")
+        
+        # self.remP = QtWidgets.QLabel()
+        # self.remP.setText("-0:00:00")
+        self._manager.songChanged.connect(self.playing.setText)
 
         self.sl = QtWidgets.QSlider(orientation=QtCore.Qt.Horizontal)
         self.sl.setFocusPolicy(QtCore.Qt.NoFocus)
         self._manager.durationChanged.connect(self.sl.setMaximum)
         self._manager.positionChanged.connect(self.sl.setValue)
 
+
+        # grid = QGridLayout()
+
         central_widget = QtWidgets.QWidget()
         self.setCentralWidget(central_widget)
         lay = QtWidgets.QVBoxLayout(central_widget)
 
         hlay = QtWidgets.QHBoxLayout()
-        for b in (quitbtn, playbtn, psebtn, stopbtn, test1, test2, test3, rand ):
+        for b in (prevbtn, playbtn, psebtn, stopbtn, nextbtn ): #prevbtn, playbtn, psebtn, stopbtn, nextbtn 
             hlay.addWidget(b)
         lay.addLayout(hlay)
         lay.addWidget(self.sl)
+        # central_widget2 = QtWidgets.QWidget()
+        # self.setCentralWidget(central_widget2)
+        # lay2 = QtWidgets.QVBoxLayout()
+        # lay2.addLayout(lay)
+        # lay2.addWidget(self.currentP)
+        lay.addWidget(self.playing)
         self.sl.valueChanged[int].connect(self._manager.setPosition)
         self.show()
+
+    def error(self):
+        quitbtn = QtWidgets.QPushButton('quit')
+        quitbtn.clicked.connect(self.close)
+
+        Error = QtWidgets.QLabel()
+        Error.setText("Error 404: No music sound")
+        central_widget = QtWidgets.QWidget()
+        self.setCentralWidget(central_widget)
+        lay = QtWidgets.QVBoxLayout(central_widget)
+        hlay = QtWidgets.QHBoxLayout()
+        hlay.addWidget(quitbtn)
+        lay.addLayout(hlay)
+        lay.addWidget(Error)
+        self.show()
+
+
+    # def keyPressEvent(self, event):
+    #     if event.key() == Qt.Key_Right:
+    #         self.sl.setValue(self.sl.value() + 5000)
+    #     elif event.key() == Qt.Key_Left:
+    #         self.s1.setValue(self.s1.value() - 5000)
+    #     else:
+    #         QWidget.keyPressEvent(self,event)
+
+
+
+
+    def getList(self):
+        scriptDir = os.path.dirname(os.path.realpath(__file__))
+        mylist = os.listdir(scriptDir + "/songs")
+        if not mylist:
+            return -1
+        else:
+            length = len(mylist)
+            for x in range(length):
+                mylist[x] = "/songs/" + mylist[x]
+            return mylist
 
 def run():
     app = QtWidgets.QApplication(sys.argv)
--- VLCManager.py
+++ VLCManager.py
@@ -1,28 +1,45 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 import vlc
-from random import *
+
+# next = "/songs/SampleAudio_0.4mb.mp3"
+
 
 class VLCManager(QtCore.QObject):
     durationChanged = QtCore.pyqtSignal(int)
     positionChanged = QtCore.pyqtSignal(int)
+    songChanged = QtCore.pyqtSignal(str)
 
-    def __init__(self, indexpl, is_playing, mylist, parent=None):
+    def __init__(self, indexpl, is_playing, mylist, mylist2, length, parent=None):
         super(VLCManager, self).__init__(parent)
         self._player = vlc.MediaPlayer()
-        self._timer = QtCore.QTimer(self, interval=100, timeout=self.update_values)
+        self._timer = QtCore.QTimer(
+            self, interval=100, timeout=self.update_values)
+        self._timer2 = QtCore.QTimer(
+            self, interval=400, timeout=self.autoPlay_next)
+        self._timer3 = QtCore.QTimer(self, interval = 100, timeout=self.update_song)
         self._indexpl = indexpl
         self._is_playing = is_playing
         self._mylist = mylist
+        self._length = length
+        self._mylist2 = mylist2
 
     def setFileName(self, filename):
         self._player = vlc.MediaPlayer(filename)
 
-    def position(self):
+    def getSong(self):
+        return self._mylist2[self._indexpl]
+    
+    @QtCore.pyqtSlot()
+    def update_song(self):
+        self.songChanged.emit(self.getSong())
+
+
+    def position(self):  # updates time when playing through file in ms
         self.durationChanged.emit(self.duration())
         return self._player.get_time()
 
-    def setPosition(self, time):
+    def setPosition(self, time):  # moving time
         if self.position() != time:
             self._player.set_time(time)
 
@@ -30,37 +47,91 @@ class VLCManager(QtCore.QObject):
     def update_values(self):
         self.positionChanged.emit(self.position())
 
-    def duration(self):
+    def duration(self):  # gets the duration of whole audio file inms
         return self._player.get_length()
+
+    def checkStopped(self):
+        if self._is_playing == 1 and self._player.is_playing == 0:
+            print("woot")
 
     @QtCore.pyqtSlot()
     def play(self):
+        self._is_playing = 1
         self._player.play()
         self.update_values()
         self._timer.start()
+        self._timer2.start()
+        self.update_song()
+        self._timer3.start()
 
     @QtCore.pyqtSlot()
     def pause(self):
         self._player.pause()
         self.update_values()
         self._timer.stop()
+        self._timer2.stop()
+        self.update_song()
+        self._timer3.stop()
 
     @QtCore.pyqtSlot()
     def stop(self):
         self._player.stop()
         self.update_values()
         self._timer.stop()
+        self._timer2.stop()
+        self.update_song()
+        self._timer3.stop()
 
-    def test1(self):
-        print(self._indexpl)
+    def autoPlay_next(self):
+        # print(self._timer2.isActive())
+        if self._player.is_playing() == 0 and self._is_playing == 1:
+            if self._indexpl == self._length - 1:  # reached end of playlist
+                self._indexpl = 0
+                self.setFileName(self._mylist[self._indexpl])
+                self.play()
+            else:
+                self._indexpl = self._indexpl + 1  # havne't reachd end of playlist
+                self.setFileName(self._mylist[self._indexpl])
+                self.play()
 
-    def test2(self):
-        print(self._is_playing)
+    def next(self):
+        self.stop()
+        if self._indexpl == self._length - 1:  # reached end of playlist
+            self._indexpl = 0
+            self.setFileName(self._mylist[self._indexpl])
+            self.play()
+        else:
+            self._indexpl = self._indexpl + 1  # havne't reachd end of playlist
+            self.setFileName(self._mylist[self._indexpl])
+            self.play()
 
-    def test3(self):
-        print(self._mylist)
+    def prev(self):
+        self.stop()
+        if self._indexpl == 0:
+            self._indexpl = self._length - 1
+            self.setFileName(self._mylist[self._indexpl])
+            self.play()
+        else:
+            self._indexpl = self._indexpl - 1
+            self.setFileName(self._mylist[self._indexpl])
+            self.play()
+        # self.stop()
+        # self.setFileName()
+        # self.play()
 
-    def randomize(self):
-        self._is_playing = randint(10, 100)
-        self._indexpl = random()
-        self._mylist = randint(0,5)
+        # self.stop()
+        # self.setFileName(self._mylist)
+        # self.play()
+
+    # def test2(self):
+    #     print(self._is_playing)
+
+    # def test3(self):
+    #     print(self._mylist)
+
+    # def keyPressed(self, event):
+
+    # def randomize(self):
+    #     self._is_playing = randint(10, 100)
+    #     self._indexpl = random()
+    #     self._mylist = randint(0,5)
